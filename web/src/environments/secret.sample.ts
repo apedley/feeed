bf68c136030fb2815506124f86d7c60d27eda962
Andrew Pedley <apedley@gmail.com>
2017-12-22 06:21:02 -0600

    Updated secret sample

---
+++ web/src/environments/secret.sample.ts
@@ -0,0 +1,22 @@
+export const secret = {
+  dev: {
+    fireBaseInfo: {
+      apiKey: '',
+      authDomain: '',
+      databaseURL: '',
+      projectId: '',
+      storageBucket: '',
+      messagingSenderId: ''
+    }
+  },
+  prod: {
+    fireBaseInfo: {
+      apiKey: '',
+      authDomain: '',
+      databaseURL: '',
+      projectId: '',
+      storageBucket: '',
+      messagingSenderId: ''
+    }
+  }  
+};
